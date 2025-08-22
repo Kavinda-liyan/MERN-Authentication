142692396105649a77550edeedce0cb3a61e9e51
Kavinda <chamarakavinda@hotmail.com>
2025-08-22 12:47:01 +0530

    chore: initialize package.json for MERN authentication project with essential dependencies and scripts

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+app.use(express.json()); //parse json
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+app.get("/", (req, res) => res.send("Server is Ready"));
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server up! listening on Port ${port}`));
